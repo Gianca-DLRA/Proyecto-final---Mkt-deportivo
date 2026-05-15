d9481b2b2b96ba73711185a8a43b0839dd4bbb47
Gianca-DLRA <giancarlo.dlra@gmail.com>
2026-05-15 12:06:03 -0600

    [UPDATE] Adicion de descricpcion en first_eda.py

---
+++ code/first_eda.py
@@ -0,0 +1,5 @@
+'''
+Script for the first EDA of the event data files. The objective of this script
+is an attempt to understand the internal structure, value behavior and overall 
+usage of the files.
+'''
